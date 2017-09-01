167bcbea17112bb494ed9bc6e232a882f926b8dd
Dex <the.escapement.codebase+DexCommits@gmail.com>
2017-09-01 10:38:21 +0100

    RC
    Final tweaks. Comment removal. Light level tweaks. Moved setup relay writes into main loop for automated setup. Concatenated debug prints on same line to same line. Changed audio file names to descriptive names.

--- pharoahs-tomb/pharoahs-tomb.ino
+++ pharoahs-tomb/pharoahs-tomb.ino
@@ -1,24 +1,23 @@
 /**
  *  Escapement Pharoah's Tomb 
  * 
- * 
  * Copyright (c) 2017, The Escapement
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are not permitted.
- *
  */
 
 // libraries used: SPI, SD, VS1053 audio
 #include <SPI.h>
 #include <SD.h>
 #include <Adafruit_VS1053.h>
-#include <RF24.h>
+#include "RF24.h"
 
 #define DEBUG 1
+
 #define LDR_TARGET_COLUMN 500
-#define LDR_TARGET_RA 70
+#define LDR_TARGET_RA 300
 
 // pin definitions
 //analogs (defined post internal hidden conversion to facilitate use)
@@ -36,20 +35,16 @@
 #define RELAY_DOOR_COL_R 34
 #define RELAY_DOOR_EXIT 36
 
-
 //common breakout and shield pins
 #define BO_RST 9
 #define BO_CS 10
 #define BO_DCS 8
-
 #define SH_RST -1
 #define SH_CS 7
 #define SH_DCS 6
 
-//sd card chip select
-#define SDCS 4
-//dreq for background music playing
-#define DREQ 3
+#define SDCS 4 //sd card chip select
+#define DREQ 3 //dreq for background music playing
 
 Adafruit_VS1053_FilePlayer audioPlayer = Adafruit_VS1053_FilePlayer(BO_RST, BO_CS, BO_DCS, DREQ, SDCS);
 
@@ -59,18 +54,11 @@ const uint64_t node[] = {0x7878787878LL, 0xB3B4B5B6F1LL, 0xB3B4B5B6CDLL, 0xB3B4B
 void setup() {
   Serial.begin(115200);
   if(DEBUG)Serial.println("Device Starting...");
-
   pinMode(RELAY_LIGHTS_MAIN, OUTPUT);
   pinMode(RELAY_LIGHTS_SPOT, OUTPUT);
   pinMode(RELAY_DOOR_COL_L, OUTPUT);
   pinMode(RELAY_DOOR_COL_R, OUTPUT);
   pinMode(RELAY_DOOR_EXIT, OUTPUT);
-  
-  digitalWrite(RELAY_LIGHTS_MAIN, HIGH);
-  digitalWrite(RELAY_LIGHTS_SPOT, LOW);
-  digitalWrite(RELAY_DOOR_COL_L, LOW);
-  digitalWrite(RELAY_DOOR_COL_R, LOW);
-  digitalWrite(RELAY_DOOR_EXIT, LOW);
   
   if (! audioPlayer.begin()){
     if(DEBUG)Serial.println(F("error: Audio Board FAILED"));
@@ -83,10 +71,7 @@ void setup() {
   if(DEBUG)Serial.println(F("SD Present"));
   audioPlayer.setVolume(10,10);
   audioPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
-  audioPlayer.playFullFile("opentone.mp3");
   if(DEBUG)Serial.println(F("Audio Board Initialised"));
-
-
   if(DEBUG)Serial.println("RADIO Starting...");
   radio.begin();
   radio.setChannel(120);
@@ -95,10 +80,16 @@ void setup() {
   for (int i = 1; i<4 ; i++) radio.openReadingPipe(i,node[i]);
   radio.startListening();
   if(DEBUG)Serial.println("RADIO success...");
-
 }
 
 void loop() {
+
+  digitalWrite(RELAY_LIGHTS_MAIN, HIGH);
+  digitalWrite(RELAY_LIGHTS_SPOT, LOW);
+  digitalWrite(RELAY_DOOR_COL_L, LOW);
+  digitalWrite(RELAY_DOOR_COL_R, LOW);
+  digitalWrite(RELAY_DOOR_EXIT, LOW);
+  audioPlayer.playFullFile("rasuccess.mp3");
 
   int columns[] = {IN_LEFT_COL, IN_RIGHT_COL};
   int maglocks[] = {RELAY_DOOR_COL_L, RELAY_DOOR_COL_R};
@@ -119,48 +110,38 @@ void loop() {
       if(readings_index >= 4){
         readings_index = 0;
       }else{
-        if(DEBUG)Serial.print("readings_index: ");if(DEBUG)Serial.println(readings_index);
+        if(DEBUG)Serial.print("readings_index: "); if(DEBUG)Serial.println(readings_index);
         readings_index++;
       }
       delay(250);
     }//end while
     delay(250);
-    if(DEBUG)Serial.println("exit");
-    if(DEBUG)Serial.print("magunlocked: ");
-    if(DEBUG)Serial.println(maglocks[i]);
+    if(DEBUG)Serial.println("exit"); if(DEBUG)Serial.print("magunlocked: "); if(DEBUG)Serial.println(maglocks[i]);
     digitalWrite(maglocks[i], HIGH); //OR LOW idk
-    audioPlayer.playFullFile("column2.mp3");  
+    audioPlayer.playFullFile("stonegrindl.mp3");  
   }//end for
 
-  //JAR LIFT FROM CUPBOARD. RA SETUP
-  while(digitalRead(IN_ANUBIS) == LOW){ //just chill till anubis canopic is lifted
-  delay(250);
-  }
-    audioPlayer.playFullFile("team.mp3");
-    audioPlayer.playFullFile("column1.mp3");
-    digitalWrite(RELAY_LIGHTS_MAIN,LOW);
-    delay(4000);
-    audioPlayer.playFullFile("laugh.mp3");
-    digitalWrite(RELAY_LIGHTS_SPOT,HIGH);
+  while(digitalRead(IN_ANUBIS) == LOW) delay(250); //just chill till anubis canopic is lifted
+  audioPlayer.playFullFile("archaeologistfarewell.mp3");
+  audioPlayer.playFullFile("stonegrindl.mp3");
+  digitalWrite(RELAY_LIGHTS_MAIN,LOW);
+  delay(4000);
+  audioPlayer.playFullFile("anubisangry.mp3");
+  digitalWrite(RELAY_LIGHTS_SPOT,HIGH);
   
   int accumulator[] = {0,0,0};
   int sensor_in[] = {IN_RA_1, IN_RA_2, IN_RA_3};    
-  while(1){     //RA   
+  while(1){
     delay(250);
     if(DEBUG)Serial.print("ra sequence.");
     for(int i=0; i<3; i++){
       if(DEBUG)Serial.print("ra "); if(DEBUG)Serial.print(i);if(DEBUG)Serial.println(" check.");
       if(! (sensor_in[i] == 0)){
         accumulator[i] = accumulator[i] + (analogRead(sensor_in[i]) - accumulator[i])/3;
-        if(DEBUG)Serial.print("accumulator for sensor ");
-        if(DEBUG)Serial.print(i);
-        if(DEBUG)Serial.print(": ");
-        if(DEBUG)Serial.println(accumulator[i]);
-                       
+        if(DEBUG)Serial.print("accumulator for sensor "); if(DEBUG)Serial.print(i); if(DEBUG)Serial.print(": "); if(DEBUG)Serial.println(accumulator[i]);
         if(accumulator[i]>LDR_TARGET_RA){
-          if(DEBUG)Serial.print("light: ");
-          if(DEBUG)Serial.println(i);
-          audioPlayer.playFullFile("light1.mp3");
+          if(DEBUG)Serial.print("light: "); if(DEBUG)Serial.println(i);
+          audioPlayer.playFullFile("rasuccess.mp3");
           sensor_in[i]= 0;
         }//end if
       }//end if
@@ -173,28 +154,27 @@ void loop() {
     }
   }//end while
   
-  //RA COMPLETE ACTIONS 
-  
   digitalWrite(RELAY_LIGHTS_SPOT,LOW);
-  audioPlayer.playFullFile("laugh.mp3");
+  audioPlayer.playFullFile("stonegrinds.mp3");
   digitalWrite(RELAY_LIGHTS_MAIN,HIGH);
-  
-  //  Sarcophogus
+
   int canopicRead = 0;
   int resetRoom = 0;
-  while(resetRoom = 0){
+  while(1){
+    if(DEBUG)Serial.println("radio loop");
     if(radio.available(0)){ //is this redundant?
+      if(DEBUG)Serial.println("radio available");
       radio.read(&canopicRead, sizeof(canopicRead));
-      Serial.println(canopicRead);
+      if(DEBUG)Serial.println(canopicRead);
       if(canopicRead == 11){
         digitalWrite(RELAY_DOOR_EXIT,HIGH);
-        audioPlayer.playFullFile("cup.mp3");
-        Serial.println("Exit. countdown to loop begun");
+        audioPlayer.playFullFile("roomexit.mp3");
+        if(DEBUG)Serial.println("Exit. countdown to loop begun");
         for(int i=180; i>0; i--){ 
-          Serial.println(i);
-          delay(1000);
+          if(DEBUG)Serial.println(i);
+          if(!DEBUG)delay(900);
+          delay(100);
         }
-        // resetRoom = 1;
         break;
       }//end if
     }//end if
